416c8b8cf8d497b8cf57ad1578705b94bbe1ca6f
Constantinescu Paul <paulconst7000@gmail.com>
2021-06-15 17:35:40 +0300

    clearing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,27 +16,6 @@ public class Main {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         final UserService userService = UserService.getInstance();
-//        userService.loadUsers();
-//        Set<User> usrs = userService.getUsers();
-//        for( User usr:usrs) {
-//            if (usr.getEmail().equals("c")) {
-//                System.out.println(usr);
-//                userService.remove(usr);
-//                System.out.println(userService.getUsers());
-//                int x = usr.getUserID();
-//                usr = new User(usr.getEmail(), usr.getPassword());
-//                usr.setCurrency(111);
-//                usr.setUserID(x);
-//                userService.add(usr);
-//                System.out.println(userService.getUsers());
-//                break;
-//            }
-//        }
-
-
-//        System.out.println(userService.getUsers());
-//        Register f = new Register();
-//        userService.loadUsers();
         Login l = new Login();
 
 
